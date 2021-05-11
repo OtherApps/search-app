748f6039347c68e70a03f38976b202b5b716e2b9
jacobo <jacobo@XPS>
2021-05-11 11:06:34 -0700

    folderstuff

---
+++ src/SearchParts.js
@@ -0,0 +1,27 @@
+import listReactFiles from 'list-react-files'
+
+
+function SearchParts() {
+    return (
+        <div>
+        {outPut}    
+        </div>
+    )
+
+
+function listAllfiles(){
+
+var outPut; 
+
+    listReactFiles(__dirname).then(files =>
+        
+        outPut = outPut + files
+
+        
+
+}
+
+
+}
+
+export default SearchParts
