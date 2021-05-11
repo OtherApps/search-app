48a3c02fffbb2e5e927bd109f5eb41cf34d4c831
jacobo <jacobo@XPS>
2021-05-10 19:46:11 -0700

    added router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import SearchF from './SearchF'
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
    
 
 	
-	<SearchF/>
+   <Router>
+   
+      
+
+        <Switch>
+          
+          <Route extact path="/search">
+            <SearchF />
+          </Route>
+          <Route extac path="/">
+            <App />
+          </Route>
+        </Switch>
+    
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
