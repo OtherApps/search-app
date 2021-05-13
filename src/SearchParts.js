f465d77e9a4621549391825f2f6ea5cc1a162dbd
jacobo <jacobo@XPS>
2021-05-13 07:59:52 -0700

    Added  read file function and  loading data

--- src/SearchParts.js
+++ src/SearchParts.js
@@ -17,17 +17,40 @@ function SearchParts() {
     return (
         <div>
     <h1>Will display files here.</h1>
-	{textFiles[0].toString()}
+	{displayAFile(textFiles[0])}
         </div>
     )
 
 
 
+function occurrences(string, subString, allowOverlapping) {
 
+    string += "";
+    subString += "";
+    if (subString.length <= 0) return (string.length + 1);
 
-function displayAFile(){
+    var n = 0,
+        pos = 0,
+        step = allowOverlapping ? 1 : subString.length;
 
+    while (true) {
+        pos = string.indexOf(subString, pos);
+        if (pos >= 0) {
+            ++n;
+            pos += step;
+        } else break;
+    }
+    return n;
+}
 
+function displayAFile(filename){
+
+fetch(filename)
+            .then(function(response){
+                return response.text();
+            }).then(function (data) {
+            console.log(data);
+        })
 
  
 
