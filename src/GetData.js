8046d3ba22c393b15dd3ff934fb1fce0f898ca5d
jacobo <jacobo@XPS>
2021-05-16 09:21:20 -0700

    fixing button

--- src/GetData.js
+++ src/GetData.js
@@ -123,7 +123,7 @@ function handleChange(e){
 	
 	
 	seriesindex= e.target.selectedIndex;
-	const seriesList = <div className="divCenter" ><select size="9" style={{overflow:"auto"}} className='seriesList' onChange={handleChange2} > {outstuff_html}</select><h1></h1><br/><button>Back</button></div>
+	const seriesList = <div className="divCenter" ><select size="9" style={{overflow:"auto"}} className='seriesList' onChange={handleChange2} > {outstuff_html}</select><h1></h1><br/><button onClick={goB}>Back</button></div>
 ;
 
 
@@ -140,6 +140,7 @@ function goB(){
 	subtitlesHide.style.display="none"
 	var seriesShow =document.getElementById('series')
 	seriesShow.style.display="block"
+console.log("has been click")
 
 }
 		function showSeries()
