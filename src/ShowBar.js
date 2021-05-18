2d1dd944972bc431b2047b0fa00712a992f38164
OtherApps <linuxnerd123@gmail.com>
2021-05-18 10:42:53 -0700

    fixing

--- src/ShowBar.js
+++ src/ShowBar.js
@@ -11,7 +11,7 @@ return(
 <ul align="center">
   <li><a class="active" href="./">Home</a></li>
   <li><a href="./Search">Search</a></li>
-  <li><a href="./Count">Count Down</a></li>
+  <li><a href="./list">Count Down</a></li>
   <li><a href="#about">About</a></li>
 </ul></td>
 </tr>
