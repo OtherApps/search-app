e9266b3efb58c3ced562e56f1ebb22fe86ffa0d2
OtherApps <linuxnerd123@gmail.com>
2021-05-26 11:08:08 -0700

    adjustments

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Welcome from './Welcome'
 import ShowBar from './ShowBar'
 import ShowCount from './ShowCount'
 import Footer from './Footer'
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,7 +23,7 @@ ReactDOM.render(
          <ShowBar/>
         <Switch>
             <Route extact path="/list">
-	
+
           <ShowCount /></Route>
           <Route extact path="/search">
             <SearchF />
