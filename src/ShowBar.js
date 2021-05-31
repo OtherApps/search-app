787127717da041b87ea7e13e60ac6a106b06a8c7
OtherApps <linuxnerd123@gmail.com>
2021-05-31 11:17:58 -0700

    Update ShowBar.js

--- src/ShowBar.js
+++ src/ShowBar.js
@@ -1,6 +1,24 @@
 import RadioF from './RadioF'
+var isVisible = false; 
 const  ShowBar = (props) => {
 // Display Menu
+
+function hideBar(){
+var menuB = document.getElementById("2")
+var current = menuB.style.visibility
+if(isVisible ===false ){
+  menuB.style.visibility ="visible"
+  isVisible= true; 
+}
+else{
+  menuB.style.visibility ="hidden"
+  isVisible=false;
+  
+}
+
+console.log("has been clicked")
+
+}
 
 return(
 <div className="specialTop" style={{color:"white"}}>
@@ -12,7 +30,10 @@ return(
 
 <td colSpan="2" style={{width:"300px"}}>
 <center>
-<font className="specialTitle">  </font>
+<font className="specialTitle"> 
+<button onClick={hideBar}>
+<i class="fa fa-bars" aria-hidden="true"></i>
+</button> </font>
 <div id="status"></div>
 
 </center>
@@ -23,13 +44,16 @@ return(
 </td>
 
 <td>
-<ul style={{color:"white",backgroundColor:"transparent"}}>
+<div id="2"  style={{color:"white",backgroundColor:"transparent",visibility:"hidden"}} >
+<ul >
   <li><a className="active" href="./">Home</a></li>
   <li><a href="./Search">Búsqueda</a></li>
   <li><a href="./list">Conteo</a></li>
 
 </ul>
 
+
+</div>
 </td>
 </tr>
 </table>
