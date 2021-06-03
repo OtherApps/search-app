276af5c70527f701288d2640b978f9953b827e94
OtherApps <linuxnerd123@gmail.com>
2021-06-03 16:52:11 -0700

    Update GetMetaData.js

--- src/GetMetaData.js
+++ src/GetMetaData.js
@@ -1,3 +1,4 @@
+// Stand alone file
 
 const base64 = require('base64-js');
 const mm = require('music-metadata');
@@ -5,25 +6,15 @@ const util = require('util');
 const fs = require('fs');
 
 function GetMetaData(newfilename){
-(async () => {
+
+if(fs.existsSync("./cover/"+metadata.common.title+".jpg"))
+{
+console.log("File already exist")
+}
+else{(async () => {
   try {
     const metadata = await mm.parseFile(newfilename);
 const data2 = (util.inspect(metadata, { showHidden: true, depth: null }));
-/*
-const data3 = data2.split("}")
-var i =0; 
-
-var parts3 = data3[20].split("data:");
-
-'data:' + props.common.picture[0].format + ';base64,' + base64js.fromByteArray(props.common.picture[0].data.data);
-
-
-console.log('data'+metadata.common.picture[0].format +';base64'+base64.fromByteArray(metadata.common.picture[0].data))
-
-
-*/
-
-
 
 const fileContents = new Buffer(metadata.common.picture[0].data, 'base64')
 fs.writeFile("./cover/"+metadata.common.title+".jpg", fileContents, (err) => {
@@ -37,7 +28,11 @@ fs.writeFile("./cover/"+metadata.common.title+".jpg", fileContents, (err) => {
   } catch (error) {
     console.error(error.message);
   }
-})();
+})();}
+
+
+
+
 
 }
 
