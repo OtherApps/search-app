df23e7312453a72c3cd0ab9de3b195baf5fd3109
OtherApps <linuxnerd123@gmail.com>
2021-06-11 09:55:47 -0700

    Update CreateDiv.js
    fixing

--- src/CreateDiv.js
+++ src/CreateDiv.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom'
 
 // This is needed   by the search function 
 //  This also sends words  to  get bolded 
+var styleTitle ={
 
+color:"white"
+
+}
 function CreateDiv(props){
     // props title, fulltext
 var findMe=props.searchMe;
 var showbutton = <button onClick={showMore}>Show more</button>
+
+var hiddenStyle=props.hiddenStyle;
+var textfix = <BoldResult allofthetext={props.fulltext} query={findMe} />
 
 
 return (
@@ -15,13 +22,13 @@ return (
 <div style={{backgroundColor:"purple", width:"50%",color:"gold",boxShadow:"2px 4px 2px black"}} >
 <center><span style={{color:"gold",width:"50%"}}>{props.title}</span></center>
 
-<div style={hiddenStyle}> {props.fulltext}<br/> </div>
-<span>{textfix}</span>
+<div style={stylesTitle}> {textfix} <br/> </div>
+
 </div>
 )
 function showMore(){
 
-    
+
 }
 }
 export default CreateDiv
